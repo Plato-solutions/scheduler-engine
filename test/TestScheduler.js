71834b8031838699cbc44b28950186cd5fc09cf7
Emmanuel Nkrumah-Sarpong <emmanuel.sarpong@platoanalytics.com>
2021-11-29 16:00:22 +0000

    test: demonstrate usage

--- test/TestScheduler.js
+++ test/TestScheduler.js
@@ -1,21 +1,24 @@
 const { expect } = require("chai")
 const { describe } = require("mocha");
 
-const { execute } = require("../src/Scheduler");
+const { execute, schedule } = require("../src/Scheduler");
 const { mockResolveAsync, mockRejectAsync } = require("./Mock");
 
 describe('execute tasks', function () {
     var count
 
     it('if it passes, it attempts once', async function () {
-        count = await execute(mockResolveAsync)
-        expect(count).equal(1)
-
+        schedule(() => {
+            return new Promise((resolve, reject) => {
+                console.log("executes task")
+                resolve(true)
+            })
+        }, "*/2 * * * * *")
     })
 
     it('if it fails, it attempts FOUR more times', async function () {
         count = await execute(mockRejectAsync)
-        expect(count).equal(5)
+        expect(count).equal(4)
     })
 
 })
